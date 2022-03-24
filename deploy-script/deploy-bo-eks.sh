9c50bc37059aef6d5f13e358296208fdded542a2
shbuild <deploy@storehub.com>
2022-03-24 15:43:37 +0800

    deploy-script

--- deploy-script/deploy-bo-eks.sh
+++ deploy-script/deploy-bo-eks.sh
@@ -53,7 +53,7 @@ docker_part() {
     echo "${DOCKER_PSW}" | docker login ${DOCKER_REG} -u ${DOCKER_USR} --password-stdin 2>/dev/null || die ">>> Login Harbor Failed"
     blue ">>> Docker build"
     if [ $app = "beep-v1-web" ] || [ $app = "ist-v1-web" ];then
-         python cicd/ci/libs/get_config.py --env ${env} --appid ${app} --cluster ${cluster}
+        /data/ops/ci/libs/get_config.py --env ${env} --appid ${app} --cluster ${cluster}
     fi
     docker build -t ${DOCKER_REG}/${env}/backoffice-v1-web:${git_tag} . || die ">>> Docker Build Failed"
     rm -f ${WORKSPACE}/.env
