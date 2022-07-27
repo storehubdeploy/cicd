80a83d6851641beb75b6585741e8d9670f32fac9
Scott <scott.li@storehub.com>
2022-07-27 16:00:19 +0800

    Update CI-related scripts

--- tools/jira_issue.py
+++ tools/jira_issue.py
@@ -9,86 +9,95 @@ import jenkins
 import requests
 from github import Github
 from jira import JIRA
+from optparse import OptionParser
 
 import ci_constants as CONSTANTS
 from utils import *
 
 # TODO: it's not same structure as beep android
+# not use now
 def get_apk_info(file):
     if not os.path.exists(file):
         print("The file does not exist: " + file)
         return '-', '-'
     with open(file, 'r') as f:
         j = json.load(f)
-        version_number = j[0]['apkData']['versionName']
-        versionCode = str(j[0]['apkData']['versionCode'])
+        version_number = j['elements'][0]['versionName']
+        versionCode = str(j['elements'][0]['versionCode'])
+        s3_url = str(j['S3_URL'])
         if os.path.exists(file):
             os.remove(file)
-        return version_number, versionCode
+        return version_number, versionCode, s3_url
 
 
 if __name__ == '__main__':
-    status = sys.argv[1]
-    issue = sys.argv[2]
-    repo = sys.argv[3]
-    GIT_BRANCH = sys.argv[4]
-    GIT_COMMIT = sys.argv[5]
-    pgy_apk_url = sys.argv[6]
-    report_url = sys.argv[7]
-    JOB_NAME = sys.argv[8]
-    BUILD_NUMBER = sys.argv[9]
-    BUILD_URL = sys.argv[10]
-    recipient_default = sys.argv[11]
-    recipient = sys.argv[12]
-
     jira_site = 'https://storehub.atlassian.net/browse'
+
+    # init parameters
+    parser=OptionParser()
+    parser.add_option("--status"           , dest="status", default="false")                    # Pipeline run status
+    parser.add_option("--repo"             , dest="repo", default="storehubnet/pos-v3-mobile")  # github repo
+    parser.add_option("--pr_branch"        , dest="PR_BRANCH")                                 # PR-1613
+    parser.add_option("--pgy_url"          , dest="pgy_apk_url")                                # pgyer url
+    parser.add_option("--report_url"       , dest="report_url")
+    parser.add_option("--app"              , dest="app", default="RN POS")
+    parser.add_option("--build_url"        , dest="BUILD_URL")                                  # PR jenkins job url
+    parser.add_option("--recipient_default", dest="recipient_default", default="t_4690699114301828")
+    parser.add_option("--recipient"        , dest="recipient", default="")
+    parser.add_option("--s3"               , dest="s3_url", default="null")
+    parser.add_option("--versionCode"      , dest="versionCode", default="null")
+    parser.add_option("--versionNum"       , dest="versionNum", default="null")
+    (options, args) = parser.parse_args()
+
+    # get github message, use 'repo' and 'PR_BEANCH'
     g = Github(CONSTANTS.GITHUB_TOKEN)
-    rep = g.get_repo(repo)
-    pr_num = int(GIT_BRANCH.split('-')[1])
+    rep = g.get_repo(options.repo)
+    pr_num = int(options.PR_BRANCH.split('-')[1])
     pr = rep.get_pull(pr_num)
-    issue_branch = pr.head.ref
 
-    version_get = None
-    file = "/data/share/android__package/apk-rnpos-fat/output.json"
-    version_number, versionCode = get_apk_info(file)
+    # github parameters
+    source_branch = pr.head.ref
+    target_branch = pr.base.ref  # master
+    data = rep.get_branch(source_branch).raw_data
+    commit_info = data['commit']['commit']['message']
 
-    features_failed = ''
-    if issue_branch.startswith("release"):
-        data = rep.get_branch(issue_branch).raw_data
-        commit_info = data['commit']['commit']['message']
-        text = format_message(
-            job_name=JOB_NAME,
-            source_branch=pr.head.ref,
-            target_branch=pr.base.ref,
-            status=status,
-            version_number=version_number,
-            versionCode=versionCode,
-            pgy_url=pgy_apk_url,
-            git_commit=GIT_COMMIT,
+    # print("\n\n"+options.s3,options.status,options.repo, options.PR_BRANCH, options.pgy_apk_url, options.report_url, options.app, options.BUILD_URL,
+    #       options.recipient_default, options.recipient ,source_branch, target_branch, commit_info, versionNum, versionCode, s3_url)
+
+    if source_branch.startswith("release"):
+        text = format_message_new(
+            status=options.status,
+            app=options.app,
+            source_branch=source_branch,
+            target_branch=target_branch,
             commit_info=commit_info,
-            report_url=report_url,
-            build_url=BUILD_URL,
+            version_number=options.versionNum,
+            versionCode=options.versionCode,
+            pgy_url=options.pgy_apk_url,
+            report_url=options.report_url,
+            build_url=options.BUILD_URL,
+            s3_url=options.s3_url,
         )
-        send_message(recipient_default, text)
+        send_message(options.recipient_default, text)
     else:
-        data = rep.get_branch(issue).raw_data
-        commit_info = data['commit']['commit']['message']
-        assignee, summary, project_id, fix_version = get_ticket_info(issue)
-        text = format_message(
-            job_name=JOB_NAME,
-            jira_site=jira_site,
-            source_branch=pr.head.ref,
-            target_branch=pr.base.ref,
-            status=status,
-            version_number=version_number,
-            versionCode=versionCode,
-            pgy_url=pgy_apk_url,
-            git_commit=GIT_COMMIT,
+        assignee, summary, project_id, fix_version = get_ticket_info(source_branch)
+        print(">>>"+assignee, summary, project_id, fix_version)
+        text = format_message_new(
+            status=options.status,
+            app=options.app,
+            source_branch=source_branch,
+            target_branch=target_branch,
             commit_info=commit_info,
-            report_url=report_url,
-            build_url=BUILD_URL,
+            version_number=options.versionNum,
+            versionCode=options.versionCode,
+            pgy_url=options.pgy_apk_url,
+            report_url=options.report_url,
+            build_url=options.BUILD_URL,
+            s3_url=options.s3_url,
+            jira_site=jira_site,
             assignee=assignee,
             summary=summary,
         )
-        recipient = recipient if recipient != 'None' else recipient_default
+        recipient = options.recipient if options.recipient != 'None' else options.recipient_default
         send_message(recipient, text)
+
