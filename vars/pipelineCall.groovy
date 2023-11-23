6b9aa01f52ca328879068e5cfb66a7d6758c1bb8
Scott <95397981+ScottStorehub@users.noreply.github.com>
2023-11-23 17:59:05 +0800

    Update pipelineCall.groovy

--- vars/pipelineCall.groovy
+++ vars/pipelineCall.groovy
@@ -432,5 +432,147 @@ def call(String type,Map map) {
                 }
             }
         }
+    } else if (type == "beepweb") {
+        pipeline {
+            agent {
+                label 'slave-03'
+            }
+
+            options {
+                timeout(time: 2, unit: 'HOURS')   // timeout on whole pipeline job
+            }
+
+            environment {
+                app = "beep-v1-web"
+                repo = 'storehubnet/beep-v1-web'
+                pr_branch = "${env.GIT_BRANCH}"
+                PR_LINK = "https://github.com/${env.repo}/pull/${env.CHANGE_ID}"
+                issue = """${sh(
+                        returnStdout: true,
+                        script: "python3.6 /data/tools/get_issue.py ${repo} ${pr_branch}"
+                ).trim()}"""
+                channel = """${sh(
+                        returnStdout: true,
+                        script: "/data/ops/ci/libs/get_config_return.py beep-pipeline fat channel ${repo} ${pr_branch}"
+                ).trim()}"""
+                channel_default = """${sh(
+                        returnStdout: true,
+                        script: "/data/ops/ci/libs/get_config_return.py beep-pipeline fat channel_default ${repo} ${pr_branch}"
+                ).trim()}"""
+                qaui_action = """${sh(
+                        returnStdout: true,
+                        script: "/data/ops/ci/libs/get_config_return.py beep-pipeline fat qaui_action ${repo} ${pr_branch}"
+                ).trim()}"""
+                qaui_branch = """${sh(
+                        returnStdout: true,
+                        script: "/data/ops/ci/libs/get_config_return.py beep-pipeline fat qaui_branch ${repo} ${pr_branch}"
+                ).trim()}"""
+                report_url = """${sh(
+                        returnStdout: true,
+                        script: "/data/ops/ci/libs/get_config_return.py beep-pipeline fat report_url ${repo} ${pr_branch}"
+                ).trim()}"""
+            }
+            stages {
+                stage('Prepare env') {
+                    steps {
+                        nodejs('v12.16.1') {
+                            script {
+                                try{
+                                    time_start=getTime()
+                                    sh 'yarn install'
+                                }
+                                catch (exc) {
+                                    status='"Prepare env run failed"'
+                                    send_message(status,"null","null","null", time_start)
+                                    sh 'exit 1'
+                                }
+                            }
+                        }
+                    }
+                }
+                stage('Static Code Detection') {
+                    steps {
+                        nodejs('v12.16.1') {
+                            script {
+                                try {
+                                    sh 'yarn lint'
+                                }
+                                catch (exc) {
+                                    status='"Static Code Detection failed"'
+                                    send_message(status,"null","null","null",time_start)
+                                    sh 'exit 1'
+                                }
+                            }
+                        }
+                    }
+                }
+                stage('Unit Test') {
+                    steps {
+                        nodejs('v12.16.1') {
+                            script {
+                                try {
+                                    sh 'yarn test:coverage'
+                                }
+                                catch (exc) {
+                                    status='"Unit Test run failed"'
+                                    send_message(status,"null","null","null",time_start)
+                                    sh 'exit 1'
+                                }
+                            }
+                        }
+                    }
+                } 
+//                stage('Deploy Service To Test23') {
+//                    options {
+//                        timeout(time: 1, unit: 'HOURS')  // timeout on this stage
+//                    }
+//                    steps {
+//                        script{
+//                            def jobBuild = build job: "02-test/test23/web-backoffice-v2-webapp-test23", parameters: [gitParameter(name: 'branch', value: "${env.issue}"), booleanParam(name: 'disallowIncomplete', value: false), string(name: 'snapshotType', value: 'formal')], propagate: false
+//                            def jobResult = jobBuild.getResult()
+//                            def jobURL = jobBuild.absoluteUrl
+//                            echo "Build app result: ${jobResult}:${jobURL}"
+//        
+//                            if (jobResult != 'SUCCESS') {
+//                                status='"Deploy Failed"'
+//                                send_message(status,"null","null","null",time_start)
+//                                sh 'exit 1'
+//                            }
+//                        }
+//                    }
+//                }
+//                stage('UI Test') {
+//                    steps {
+//                        script {
+//                            def jobBuild = build job: '00-QA/qa_automation_UI-test-web', parameters: [gitParameter(name: 'branch', value: "${env.qaui_branch}"), string(name: 'actiontags', value: "${env.qaui_action}")], propagate: false
+//                            def jobResult = jobBuild.getResult()
+//                            echo "Build of 'qaui_test' result: ${jobResult}"
+//        
+//                            if (jobResult != 'SUCCESS') {
+//                                status='"UI Test Failed"'
+//                                send_message(status,"null","null","null",time_start)
+//                                sh 'exit 1'
+//                            }
+//                        }
+//                    }
+//                }
+            }
+
+            post {
+                always {
+                    cleanWs()
+                    echo 'jira sendbuildinfo1'
+                }
+                success {
+                    echo "success"
+                    send_message('success',"null","null","null",time_start)
+//                     sh "python3.6 /data/tools/change_jira.py ${env.issue}  'CI pipeline success' 'Ready_For_Test'"
+                }
+                failure {
+                    echo "failed"
+//                     sh "python3.6 /data/tools/change_jira.py ${env.issue}  'CI pipeline failed' 'Not_ready'"
+                }
+            }
+        }
     }
 }
